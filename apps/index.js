906434c7bbfd37c4b47d8f854fbbd3c499e83fbb
duckie-jr <0sigma.balls0@gmail.com>
2026-05-25 18:14:44 -0500

    Add multiple app imports to index.js

---
+++ apps/index.js
@@ -0,0 +1,12 @@
+// Add one import line here when adding a new app. Nothing else needs to change.
+import './clock.js';
+import './weather.js';
+import './calculator.js';
+import './notes.js';
+import './todo.js';
+import './calendar.js';
+import './music.js';
+import './browser.js';
+import './settings.js';
+import './playstore.js';
+import './games/snake.js';
